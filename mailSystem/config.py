2e4f4a80d944185afaf0e9d783a1f4c99a240d87
jayesh <jffjayesh@gmail.com>
2022-08-13 17:00:09 +0530

    postgres --> postgresql

--- mailSystem/config.py
+++ mailSystem/config.py
@@ -1,9 +1,13 @@
 import os
+import re
 
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri and uri.startswith("postgres://"):
+    val = uri.replace("postgres://", "postgresql://", 1)
 
 class Config:
     # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:hr@localhost/mailingsys'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = val
     # MAKE DEBUG FALSE
     DEBUG = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
