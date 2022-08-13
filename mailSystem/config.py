f11cd49a271f962b578b1bee08f27fb55dc1ce3c
jayesh <jffjayesh@gmail.com>
2022-08-13 15:41:23 +0530

    debug : false

--- mailSystem/config.py
+++ mailSystem/config.py
@@ -3,6 +3,6 @@
 class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:hr@localhost/mailingsys'
     # MAKE DEBUG FALSE
-    DEBUG = True
+    DEBUG = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_TYPE = 'sqlalchemy'
